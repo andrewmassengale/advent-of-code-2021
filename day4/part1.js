816a91ea162d7f4c0e1b6f0fab2eddd93d05b8a5
Andrew Massengale <andrew.massengale@gmail.com>
2021-12-05 11:01:09 -0600

    day 4

---
+++ day4/part1.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileData = fs.readFileSync(path.join(__dirname, 'input.txt'));
+const data = fileData
+	.toString()
+	.split('\r\n');
+
+const boardSize = 5;
+
+const balls = data[0].split(',').map((num) => parseInt(num, 10));
+
+let boards = data.slice(2).reduce((boards, line) => {
+	if (line === '') {
+		return boards;
+	}
+	let lastBoardIdx = boards.length - 1;
+	if (boards[lastBoardIdx].nums.length === boardSize * boardSize) {
+		boards.push({ nums: [ ], lines: [ ] });
+		const verts = [ ];
+		for (let i = 0; i < boardSize; verts[i++] = [ ]);
+		boards[lastBoardIdx].lines.forEach((line) => {
+			line.forEach((num, numIdx) => {
+				verts[numIdx].push(num);
+			});
+		});
+		boards[lastBoardIdx].lines = boards[lastBoardIdx].lines.concat(verts);
+		++lastBoardIdx;
+	}
+	const nums = line.split(' ').filter(Boolean).map((num) => parseInt(num, 10));
+	boards[lastBoardIdx].nums = boards[lastBoardIdx].nums.concat(nums);
+	boards[lastBoardIdx].lines.push(nums);
+	return boards;
+}, [ { nums: [ ], lines: [ ] } ]);
+
+let found = 0;
+balls.every((ball) => {
+	boards = boards.map((board) => {
+		const ballNumIdx = board.nums.indexOf(ball);
+		if (ballNumIdx !== -1) {
+			board.nums.splice(ballNumIdx, 1);
+		}
+
+		board.lines = board.lines.map((line) => {
+			ballLineIdx = line.indexOf(ball);
+			if (ballLineIdx !== -1) {
+				line.splice(ballLineIdx, 1);
+			}
+			if (line.length === 0) {
+				found = board.nums.reduce((curSum, num) => curSum + num, 0) * ball;
+			}
+
+			return line;
+		});
+
+		return board;
+	});
+
+	return (found === 0);
+});
+
+console.log(found);
